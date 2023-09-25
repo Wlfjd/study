da4715f8d730260f837d095ef4d8751250264826
Wlfjd <leejiwon813@naver.com>
2023-09-26 00:39:21 +0900

    ajax 구현

--- tab.js
+++ tab.js
@@ -32,15 +32,55 @@ var car2 = { name: '소나타', price: [50000, 3000, 4000] }
 $('.name').html(car2.name)
 $('.price').html(car2.price[1])
 
+let shirts = [90, 95, 100, 105];
+let pants = [28, 30, 32, 34];
 
 $('.form-select').eq(0).on('change', function () {
     if (this.value == '셔츠') {
         $('.form-select').eq(1).removeClass('form-hide')
+        $('.form-select').eq(1).html('')
+        shirts.forEach((data) => {
+            $('.form-select').eq(1).append(`<option>${data}</option>`)
+        })
+    } else if (this.value == '바지') {
+        $('.form-select').eq(1).html('')
+        pants.forEach((data) => {
+            $('.form-select').eq(1).removeClass('form-hide')
+            $('.form-select').eq(1).append(`<option>${data}</option>`)
+        })
     } else {
         $('.form-select').eq(1).addClass('form-hide')
     }
 })
 
-let p = document.createElement('p'); // 태그 생성
-p.innerHTML = '안녕' // html 생성
-document.querySelector('#test').append(p)
+
+// let p = document.createElement('p'); // 태그 생성
+// p.innerHTML = '안녕' // html 생성
+// document.querySelector('#test').appendChild(p)
+
+let p = '<p>안녕하세요</p>'
+document.querySelector('#test').insertAdjacentHTML('beforeend', p)
+$('#test').append(p)
+
+let obj = { name: 'kim', age: 30 }
+
+for (let key in obj) [
+    console.log(obj[key])
+]
+
+
+function Score(befores, newScore) {
+    let sum = 0;
+    let avg = 0;
+    befores.forEach(function (data) {
+        sum += data;
+    })
+    avg = sum / befores.length
+    if (avg < newScore) {
+        console.log(`평균보다 ${newScore - avg}점이 올랐네요`)
+    } else {
+        console.log(`평균보다 ${avg - newScore}점이 떨어졌네요`)
+
+    }
+}
+Score([10, 20, 30, 40, 50], 40)
