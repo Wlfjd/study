0708fdf604ff99fd9c217bac233265acb5d0900d
Wlfjd <leejiwon813@naver.com>
2023-10-12 22:33:04 +0900

    typescript 타입정의 연습

--- index2.ts
+++ index2.ts
@@ -1,1 +1,20 @@
-let name = "kim";
+// let name1: string[] = ["kim", "park"];
+// let name2: { name?: string } = { name: "kim" };
+// let name3: string | number = "kim";
+
+// function func(x: number): number {
+//   return x * 2;
+// }
+
+// type Member1 = [number, boolean];
+// let john: Member1 = [123, true];
+// console.log(john);
+
+// type Member2 = {
+//   name: string;
+// };
+// let sam: Member2 = { name: "kim" };
+
+let names: string = "kim";
+let ages: number = 50;
+let marrige = true;
