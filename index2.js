44d31a6661c8edad37ca42f60f85f748477d6847
Wlfjd <leejiwon813@naver.com>
2023-10-15 22:45:45 +0900

    union, any,unknown 타입과 연습문제 공부

--- index2.js
+++ index2.js
@@ -14,3 +14,24 @@
 var names = "kim";
 var ages = 50;
 var marrige = true;
+//union 타입
+var members = [1, "2", 3];
+var member2 = { a: "123" };
+//any 타입 (타입실드 해제 문법) -> 모든 자료형 허용해줘서 타입스크립트의 장점 사라짐
+var text = "123";
+//unknown 타입 -> any랑 비슷하지만 더 안전함
+var text2 = 1;
+// text2 - 1; //타입이 정확하지 않아서 에러 발생
+//practice1
+var user = "kim";
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+//practice2
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: "Phil",
+    friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
--- index2.ts
+++ index2.ts
@@ -18,3 +18,33 @@
 let names: string = "kim";
 let ages: number = 50;
 let marrige = true;
+
+//union 타입
+let members: (number | string)[] = [1, "2", 3];
+let member2: { a: string | number } = { a: "123" };
+
+//any 타입 (타입실드 해제 문법) -> 모든 자료형 허용해줘서 타입스크립트의 장점 사라짐
+let text: any = "123";
+
+//unknown 타입 -> any랑 비슷하지만 더 안전함
+let text2: unknown = 1;
+// text2 - 1; //타입이 정확하지 않아서 에러 발생
+
+//practice1
+let user: string = "kim";
+let age: number | undefined = undefined;
+let married: boolean = false;
+let 철수: (string | number | undefined | boolean)[] = [user, age, married];
+
+//practice2
+let 학교: {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string | string[];
+} = {
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
